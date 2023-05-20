e119d9c056d2a8b37913209423bb7292025397a3
almiluk <almiluk@gmail.com>
2023-05-20 19:03:41 +0500

    chapter2: rewrite for pre-defense (WiP)

---
+++ report/resources/code_listings/EEPROManager-var-example.cpp
@@ -0,0 +1,15 @@
+EEPROMVar<uint16_t> int_var("int_var", 1);
+
+int_var = int_var + 1;
+...
+int_var.UpdateNow();
+
+EEPROMVar<UserClass> custom_data("custom_data", UserClass());
+
+custom_data.Data().UpdateField();
+
+custom_data.Update();
+
+custom_data = custom_data.Data().UpdateField();
+
+
