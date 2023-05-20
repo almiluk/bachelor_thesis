404c4c40ce51a4f9075782fdb92dae1b05174842
almiluk <almiluk@gmail.com>
2023-05-20 23:53:28 +0500

    chapter2: first version

--- report/resources/code_listings/EEPROManager-var-example.cpp
+++ report/resources/code_listings/EEPROManager-var-example.cpp
@@ -1,7 +1,7 @@
 EEPROMVar<uint16_t> int_var("int_var", 1);
 
 int_var = int_var + 1;
-...
+
 int_var.UpdateNow();
 
 EEPROMVar<UserClass> custom_data("custom_data", UserClass());
@@ -11,5 +11,3 @@ custom_data.Data().UpdateField();
 custom_data.Update();
 
 custom_data = custom_data.Data().UpdateField();
-
-
